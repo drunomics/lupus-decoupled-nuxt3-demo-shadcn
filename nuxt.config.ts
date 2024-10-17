0d0cc82665ef76070ba5f4425da6b1003fb70368
Matic Fink <turtlman@gmail.com>
2024-10-17 09:32:49 +0200

    Add shadcn.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxtjs-drupal-ce', "@nuxt/eslint"],
+  modules: [
+    'nuxtjs-drupal-ce',
+    "@nuxt/eslint",
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+  ],
 
   drupalCe: {
     drupalBaseUrl: 'http://lupus-decoupled.ddev.site',
