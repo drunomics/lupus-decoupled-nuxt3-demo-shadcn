b691708425a8d4a326f70ac84b76b47f3575a8fa
Alexandru <videoloss3@gmail.com>
2024-12-23 13:38:58 +0200

    LDP-2673: Small fixes

--- components/global/node-blog-teaser.vue
+++ components/global/node-blog-teaser.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link class="node" :to="path.pathAlias">
     <h2 v-if="title" class="mb-2 text-3xl font-semibold">{{ title }}</h2>
-    <div v-if="featuredImage" class="prose max-w-none" v-html="featuredImage.mediaImage.content" />
+    <div v-if="featuredImage && featuredImage.mediaImage" class="prose max-w-none" v-html="featuredImage.mediaImage.content" />
   </nuxt-link>
 </template>
 
